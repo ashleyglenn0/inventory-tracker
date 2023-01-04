65c08d5fa7face8b51a9af3f462c55f389042c04
mgrumbine <you@example.com>
2023-01-04 10:33:22 -0700

    Made some changes to build.gradle.kts to see if I could fix it

--- inventory-tracker-gradle/build.gradle.kts
+++ inventory-tracker-gradle/build.gradle.kts
@@ -1,21 +1,31 @@
 plugins {
 	java
-	id("org.springframework.boot") version "2.7.7"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id 'org.springframework.boot' version '2.7.7'
+	id 'io.spring.dependency-management' version '1.0.15.RELEASE'
 }
 
-group = "org.launchcode"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+group = 'org.launchcode'
+version = '0.0.1-SNAPSHOT'
+//java.sourceCompatibility = JavaVersion.VERSION_11
+sourceCompatibility = '11'
+
+configurations { //is this necessary?
+	developmentOnly
+	runtimeClasspath {
+		extendsFrom developmentOnly
+	}
+}
 
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation 'org.springframework.boot:spring-boot-starter-web'
+	implementation 'org.springframework.boot:spring-boot-starter-data-jpa'
+	implementation 'mysql:mysql-connector-java'
+	developmentOnly 'org.springframework.boot:spring-boot-devtools'
+	testImplementation('org.springframework.boot:spring-boot-starter-test')
 }
 
 tasks.withType<Test> {
