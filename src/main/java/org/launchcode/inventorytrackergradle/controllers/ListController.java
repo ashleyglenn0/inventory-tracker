3c72119ddf70025937a6df474d76ed5e72fb01bb
mgrumbine <you@example.com>
2023-01-11 15:54:20 -0700

    Edited based on Suna and Kim's comments to my pull request.

--- src/main/java/org/launchcode/inventorytrackergradle/controllers/ListController.java
+++ src/main/java/org/launchcode/inventorytrackergradle/controllers/ListController.java
@@ -5,14 +5,14 @@ import org.launchcode.inventorytrackergradle.models.ItemData;
 import org.launchcode.inventorytrackergradle.repositories.ItemRepository;
 import org.launchcode.inventorytrackergradle.repositories.ManufacturerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
-@Controller
+@RestController
+//@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "list")
 public class ListController {
 
@@ -22,34 +22,15 @@ public class ListController {
     @Autowired
     private ManufacturerRepository manufacturerRepository;
 
-    static HashMap<String, String> columnChoices = new HashMap<>();
-
-    public ListController () {
-
-        columnChoices.put("all", "All");
-        columnChoices.put("manufacturer", "Manufacturer");
-
-    }
-
-    @RequestMapping("")
-    public String list(Model model) {
-        model.addAttribute("manufacturers", manufacturerRepository.findAll());
-        return "list";
-    }
-
     @RequestMapping(value = "items")
-    public String listJobsByColumnAndValue(Model model, @RequestParam String column, @RequestParam String value) {
+    public Iterable<Item> listJobsByColumnAndValue(@RequestParam String column, @RequestParam String value) {
         Iterable<Item> items;
-        if (column.toLowerCase().equals("all")){
+        if (column.toLowerCase().equals("all")) {
             items = itemRepository.findAll();
-            model.addAttribute("title", "All Items");
         } else {
             items = ItemData.findByColumnAndValue(column, value, itemRepository.findAll());
-            model.addAttribute("title", "Items with " + columnChoices.get(column) + ": " + value);
         }
-        model.addAttribute("items", items);
-
-        return "list-jobs";
-    }
-
+            return items;
+        }
 }
+
