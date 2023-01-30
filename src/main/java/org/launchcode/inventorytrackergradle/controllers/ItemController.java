02de682703519e4fcaf3b8177608ea12bb06e34f
kimlee <kiim.n.lee@gmail.com>
2023-01-30 11:34:37 -0600

    added putmapping method to update entire item and added patchmap method to update the item number in inventory. tested in postman.

--- src/main/java/org/launchcode/inventorytrackergradle/controllers/ItemController.java
+++ src/main/java/org/launchcode/inventorytrackergradle/controllers/ItemController.java
@@ -3,6 +3,8 @@ package org.launchcode.inventorytrackergradle.controllers;
 import org.launchcode.inventorytrackergradle.models.Item;
 import org.launchcode.inventorytrackergradle.models.data.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -13,6 +15,10 @@ import java.util.Optional;
 public class ItemController {
     @Autowired
     private ItemRepository itemRepository;
+    //added this part in
+    ItemController(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
 
     @GetMapping("")
     public Iterable<Item> getItems() {return itemRepository.findAll(); }
@@ -33,5 +39,35 @@ public class ItemController {
     @DeleteMapping("{id}")
     void deleteItem (@PathVariable int id){
         itemRepository.deleteById(id);
+    }
+
+// replaces whole item, so would need to pass in all fields (aka the whole object). if we use this, consider way to have fields auto-filled  with current data so user only needs to change what they want
+    @PutMapping("/{id}")
+    Item replaceItem(@RequestBody Item newItem, @PathVariable Integer id){
+        Optional<Item> itemToReplace = itemRepository.findById(id);
+        if (itemToReplace.isPresent()){
+            Item itemData =  itemToReplace.get();
+            itemData.setName(newItem.getName());
+            itemData.setCategory(newItem.getCategory());
+            itemData.setDescription(newItem.getDescription());
+            itemData.setNumberInInventory(newItem.getNumberInInventory());
+            itemData.setNumberMinimumToKeepOnHand(newItem.getNumberMinimumToKeepOnHand());
+            return itemRepository.save(itemData);
+        } else {
+            return itemRepository.save(newItem); //may need to update this line - 500 error occurs if invalid id is passed
+        }
+    }
+
+    //updates number in inventory based on path variable rather than passing in an object
+    @PatchMapping("/{id}/{newNumberInInventory}")
+    ResponseEntity<Item> updateNumberInInventory(@PathVariable Integer id, @PathVariable int newNumberInInventory) {
+        Optional<Item> itemToUpdate = itemRepository.findById(id);
+        if (itemToUpdate.isPresent()){
+            Item itemData =  itemToUpdate.get();
+            itemData.setNumberInInventory(newNumberInInventory);
+            return new ResponseEntity<Item>(itemRepository.save(itemData), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
