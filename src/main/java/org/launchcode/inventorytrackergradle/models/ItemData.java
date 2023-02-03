1a50b270e22635edc3e4ac61a16b9b57a56b972d
mgrumbine <you@example.com>
2023-02-03 11:49:50 -0700

    Fixed it so search will work with only a searchTerm. It no longer requires both searchTerm and searchTyoe.

--- src/main/java/org/launchcode/inventorytrackergradle/models/ItemData.java
+++ src/main/java/org/launchcode/inventorytrackergradle/models/ItemData.java
@@ -4,7 +4,48 @@ import java.util.ArrayList;
 
 public class ItemData {
 
-    public static ArrayList<Item> findByColumnAndValue(String column, String value, Iterable<Item> allItems) {
+//    public static ArrayList<Item> findByColumnAndValue(String column, String value, Iterable<Item> allItems) {
+//
+//        ArrayList<Item> results = new ArrayList<>();
+//
+//        if (value.toLowerCase().equals("all")){
+//            return (ArrayList<Item>) allItems;
+//        }
+//
+//        if (column.equals("all")){
+//            results = findByValue(value, allItems);
+//            return results;
+//        }
+//        for (Item item : allItems) {
+//
+//            String aValue = getFieldValue(item, column);
+//
+//            if (aValue != null && aValue.toLowerCase().contains(value.toLowerCase())) {
+//                results.add(item);
+//            }
+//        }
+//        return results;
+//    }
+//
+//    public static String getFieldValue(Item item, String fieldName){
+//        String theValue;
+//        if (fieldName.equals("name")){
+//            theValue = item.getName();
+//        } else if (fieldName.equals("manufacturer")){
+//            Manufacturer manufacturer = item.getManufacturer();
+//            theValue = manufacturer.getName();
+//        } else if (fieldName.equals("description")){
+//            theValue = item.getDescription();
+//        } else if (fieldName.equals("category")) {
+//            theValue = item.getCategory();
+//        } else {
+//            theValue = "Invalid field name! Backend only set up to search by name, manufacturer, description, or category. ItemData, line 41.";
+//            System.out.println(theValue);
+//        }
+//        return theValue;
+//    }
+
+    public static ArrayList<Item> findByValue(String value, Iterable<Item> allItems) {
 
         ArrayList<Item> results = new ArrayList<>();
 
@@ -12,53 +53,17 @@ public class ItemData {
             return (ArrayList<Item>) allItems;
         }
 
-        if (column.equals("all")){
-            results = findByValue(value, allItems);
-            return results;
-        }
         for (Item item : allItems) {
-
-            String aValue = getFieldValue(item, column);
-
-            if (aValue != null && aValue.toLowerCase().contains(value.toLowerCase())) {
-                results.add(item);
-            }
-        }
-        return results;
-    }
-
-    public static String getFieldValue(Item item, String fieldName){
-        String theValue;
-        if (fieldName.equals("name")){
-            theValue = item.getName();
-        } else if (fieldName.equals("manufacturer")){
             Manufacturer manufacturer = item.getManufacturer();
-            theValue = manufacturer.getName();
-        } else if (fieldName.equals("description")){
-            theValue = item.getDescription();
-        } else if (fieldName.equals("category")) {
-            theValue = item.getCategory();
-        } else {
-            theValue = "Invalid field name! Backend only set up to search by name, manufacturer, description, or category. ItemData, line 41.";
-            System.out.println(theValue);
-        }
-        return theValue;
-    }
-
-    public static ArrayList<Item> findByValue(String value, Iterable<Item> allItems) {
-        String lower_val = value.toLowerCase();
-
-        ArrayList<Item> results = new ArrayList<>();
-
-        for (Item item : allItems) {
-
-            if (item.getName().toLowerCase().contains(lower_val)) {
+            System.out.println(manufacturer);
+            if (item.getName().toLowerCase().contains(value.toLowerCase())) {
                 results.add(item);
-            } else if (item.getManufacturer().toString().toLowerCase().contains(lower_val)) {
+                System.out.println("Line 61");
+            } else if (manufacturer.getName().toString().toLowerCase().contains(value.toLowerCase())) {
                 results.add(item);
-            } else if (item.getDescription().toString().toLowerCase().contains(lower_val)) {
+            } else if (item.getDescription().toLowerCase().contains(value.toLowerCase())) {
                 results.add(item);
-            } else if (item.toString().toLowerCase().contains(lower_val)) {
+            } else if (item.getCategory().toLowerCase().contains(value.toLowerCase())) {
                 results.add(item);
             }
 
